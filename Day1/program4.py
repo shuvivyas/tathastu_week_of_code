d0f975d8bc59424a420ba172dac3ff627e401c28
shuvivyas <66160119+shuvivyas@users.noreply.github.com>
2020-05-30 14:38:25 +0530

    Create program4.py

---
+++ Day1/program4.py
@@ -0,0 +1,6 @@
+SP=float(input ("Enter selling price "))
+CP=float(input ("Enter cost price "))
+profit=SP-CP
+print("Profit of the sell ",profit)
+sprice=(1.05*profit)+CP
+print("New selling price ",sprice)
